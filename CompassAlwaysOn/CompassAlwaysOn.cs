8cc45aaaab330f77c91d01d8c04906c2c81c8938
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2021-09-19 20:30:26 +0100

    HKMP compatibility

--- CompassAlwaysOn/CompassAlwaysOn.cs
+++ CompassAlwaysOn/CompassAlwaysOn.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using Modding;
 using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using Mono.Cecil.Cil;
 
 namespace CompassAlwaysOn
@@ -13,12 +15,26 @@ namespace CompassAlwaysOn
         internal static CompassAlwaysOn instance;
         public const string EnabledBool = "CompassAlwaysOn_enabled";
 
+
+        private static ILHook _HKMPHook;
         public override void Initialize()
         {
             instance = this;
 
             IL.GameMap.PositionCompass += ModifyCompassBool;
             IL.GameMap.WorldMap += ModifyCompassBool;
+
+            if (ModHooks.GetMod("HKMP") is Mod hkmp)
+            {
+                Log("Attempting to hook HKMP");
+                Type HkmpMapManager = Type.GetType("Hkmp.Game.Client.MapManager, Hkmp");
+                MethodInfo HkmpHeroUpdateMethod = HkmpMapManager?.GetMethod("HeroControllerOnUpdate", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (HkmpHeroUpdateMethod != null)
+                {
+                    Log("Hooking HKMP");
+                    _HKMPHook = new ILHook(HkmpHeroUpdateMethod, ModifyCompassBool);
+                }
+            }
 
             ModHooks.GetPlayerBoolHook += InterpretCompassBool;
         }
@@ -47,6 +63,7 @@ namespace CompassAlwaysOn
         {
             IL.GameMap.PositionCompass -= ModifyCompassBool;
             IL.GameMap.WorldMap -= ModifyCompassBool;
+            _HKMPHook?.Dispose();
 
             ModHooks.GetPlayerBoolHook -= InterpretCompassBool;
         }
